06fcc06d533e93476dd9b0ac90e71aba46be37b3
Vilius <vilius.andraitis@gmail.com>
2021-09-30 15:10:15 +0300

    Revert "Added implemented assigned unit tests" This reverts commit c3c4c403d4a39f91dae637e8fb9b74087009317c.
    "Undoing commit"

--- src/test/java/EmailTest.java
+++ src/test/java/EmailTest.java
@@ -1,35 +1,34 @@
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import src.com.validators.EmailValidator;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class EmailTest {
-//    EmailValidator emailValidator;
-//
-//    @BeforeEach
-//    void setUp() {
-//        emailValidator = new EmailValidator();
-//    }
-//
-//    @Test
-//    void TestContainsAtSymbol() {
-//        assertTrue(emailValidator.containsAtSymbol("asd123@test"));
-//    }
-//
-//    @Test
-//    void TestContainsInadmissibleSymbol() {
-//        assertTrue(emailValidator.containsInadmissibleSymbol("asd123©"));
-//    }
-//
-//    @Test
-//    void TestHasBadDomain() {
-//        assertTrue(emailValidator.hasBadDomain("asd123@com"));
-//    }
-//
-//    @Test
-//    void TestHasBadTopLevelDomain() {
-//        assertTrue(emailValidator.hasBadTopLevelDomain("asd123@test.asdasd"));
-//    }
-//
-//}
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EmailTest {
+    EmailValidator emailValidator;
+
+    @BeforeEach
+    void setUp() {
+        emailValidator = new EmailValidator();
+    }
+
+    @Test
+    void TestContainsAtSymbol() {
+        assertTrue(emailValidator.containsAtSymbol("asd123@test"));
+    }
+
+    @Test
+    void TestContainsInadmissibleSymbol() {
+        assertTrue(emailValidator.containsInadmissibleSymbol("asd123©"));
+    }
+
+    @Test
+    void TestHasBadDomain() {
+        assertTrue(emailValidator.hasBadDomain("asd123@com"));
+    }
+
+    @Test
+    void TestHasBadTopLevelDomain() {
+        assertTrue(emailValidator.hasBadTopLevelDomain("asd123@test.asdasd"));
+    }
+
+}
